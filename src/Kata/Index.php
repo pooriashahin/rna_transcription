4b7cd970b7310344d19af91fe4138903f4315444
pooria <pooria@drsnwk.com>
2021-02-17 00:52:43 -0500

    initial test completed

--- src/Kata/Index.php
+++ src/Kata/Index.php
@@ -4,8 +4,20 @@ namespace Kata;
 
 class Index
 {
-    public function handle(string $letter): string
+    public function handle(string $nucleotide): string
     {
+        if ($nucleotide === 'T')
+        {
+            return 'A';
+        }
+        if ($nucleotide === 'G')
+        {
+            return 'C';
+        }
+        if ($nucleotide === 'C')
+        {
+            return 'G';
+        }
         return 'U';
     }
 
--- tests/Kata/IndexTest.php
+++ tests/Kata/IndexTest.php
@@ -14,12 +14,32 @@ class IndexTest extends TestCase
 
     public function testABecomesU(): void 
     {
-
         $actual = $this->index->handle('A');
         $expected = 'U';
-        
         $this->assertSame($expected, $actual);
     }
+
+    public function testCBecomesG(): void 
+    {
+        $actual = $this->index->handle('C');
+        $expected = 'G';
+        $this->assertSame($expected, $actual);
+    }
+
+    public function testGBecomesC(): void 
+    {
+        $actual = $this->index->handle('G');
+        $expected = 'C';
+        $this->assertSame($expected, $actual);
+    }
+
+    public function testTBecomesA(): void 
+    {
+        $actual = $this->index->handle('T');
+        $expected = 'A';
+        $this->assertSame($expected, $actual);
+    }
+
 
  
 }
