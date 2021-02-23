b6266bb46d98d030df824354ab9b0a5976072dfb
pooria <pooria@drsnwk.com>
2021-02-23 16:14:49 -0500

    i

---
+++ src/Kata/RnaNucleotide.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Kata;
+
+
+class RnaNucleotide {
+
+    private string $value;
+
+    public function __construct(string $rnaNucleotide) {
+
+        $this->value = $rnaNucleotide;
+    }
+
+
+
+
+
+
+
+
+
+
+}
