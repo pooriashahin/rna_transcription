4c28f0b3f954c90699f9138cdd9368c88e4784ef
danielescillia <daniele.scillia@gmail.com>
2021-02-23 23:07:37 +0100

    isequals logic implemented for DnaNucleotide

--- src/Kata/DnaNucleotide.php
+++ src/Kata/DnaNucleotide.php
@@ -2,28 +2,18 @@
 
 namespace Kata;
 
-
 class DnaNucleotide {
 
     private string $value;
 
     public function __construct(string $dnaNucleotide) {
-
         $this->value = $dnaNucleotide;
     }
 
-    public function isEqual(DnaNucleotide $dnaNucleotide): bool
+    public function isEquals(DnaNucleotide $dnaNucleotide): bool
     {
-      return $this->value === $dnaNucleotide->value;
+        return $this->value === $dnaNucleotide->value;
     }
 
-
-
-
-
-
-
-
-
-
 }
+
